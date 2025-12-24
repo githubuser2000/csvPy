e746e580155b2c9fc040cb17e6e1b67b542e043a
Alexander Kern <alexgit@alexkern.de>
2025-12-24 16:24:29 +0100

    econs

---
+++ econs/menue/build/CMakeFiles/econ49.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "/data/data/com.termux/files/home/myRepos/csvPy/econs/menue/bin/econ49"
+  "/data/data/com.termux/files/home/myRepos/csvPy/econs/menue/bin/econ49.pdb"
+  "CMakeFiles/econ49.dir/link.d"
+  "CMakeFiles/econ49.dir/src/bitmask.cpp.o"
+  "CMakeFiles/econ49.dir/src/bitmask.cpp.o.d"
+  "CMakeFiles/econ49.dir/src/main.cpp.o"
+  "CMakeFiles/econ49.dir/src/main.cpp.o.d"
+  "CMakeFiles/econ49.dir/src/mode_ncurses.cpp.o"
+  "CMakeFiles/econ49.dir/src/mode_ncurses.cpp.o.d"
+  "CMakeFiles/econ49.dir/src/mode_text.cpp.o"
+  "CMakeFiles/econ49.dir/src/mode_text.cpp.o.d"
+  "CMakeFiles/econ49.dir/src/mode_toggle.cpp.o"
+  "CMakeFiles/econ49.dir/src/mode_toggle.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/econ49.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ econs/menue/include/mode_text.h
@@ -0,0 +1,6 @@
+#ifndef MODE_TEXT_H
+#define MODE_TEXT_H
+
+void mode_text();
+
+#endif
